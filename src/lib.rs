08bba3e34fe99efd8a4f55b6fd93edf0ea1ae7fa
dablenparty <hdavenport8902@gmail.com>
2022-10-13 23:11:29 -0400

    Created platform_specific_executable_name

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,28 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use const_format::formatcp;
+use std::env;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// Gets a platform-specific executable name based on the `CARGO_PKG_NAME` environment variable.
+///
+/// This function is generated at compile-time and can be used in `const` contexts.
+///
+/// # Examples
+///
+/// ```
+/// # use std::env::consts::*;
+/// use dablenutil::platform_specific_executable_name;
+/// # const CARGO_PKG_NAME: &str = env!("CARGO_PKG_NAME");
+///
+/// let executable_name = platform_specific_executable_name();
+/// let expected_name = format!("{}_{}_{}{}", CARGO_PKG_NAME, OS, ARCH, EXE_SUFFIX);
+/// assert_eq!(expected_name, executable_name);
+/// ```
+pub const fn platform_specific_executable_name() -> &'static str {
+    const PACKAGE_NAME: &str = env!("CARGO_PKG_NAME");
+    formatcp!(
+        "{}_{}_{}{}",
+        PACKAGE_NAME,
+        env::consts::OS,
+        env::consts::ARCH,
+        env::consts::EXE_SUFFIX
+    )
 }
